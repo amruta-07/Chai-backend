a6472794073972126d2b0319566591a5be0b5a03
amruta-07 <misalamruta08@gmail.com>
2024-06-05 23:06:29 +0530

    custom api response and error handling

--- src/db/index.js
+++ src/db/index.js
@@ -1,14 +1,14 @@
 import mongoose from 'mongoose'
 import { DB_NAME } from '../constant.js'
 
-const connectDB= async ()=>{
-    try{
-      const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-      console.log(`MongoDB connected!! DB Host: ${connectionInstance.connection.host}`)
-    }catch (error){
-        console.log("MongoDB connection error", error)
-        process.exit(1)
-    }
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+    console.log(`MongoDB connected!! DB Host: ${connectionInstance.connection.host}`)
+  } catch (error) {
+    console.log("MongoDB connection error", error)
+    process.exit(1)
+  }
 
 
 }
